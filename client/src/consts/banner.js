99bafebd5d539fd8bd03c142ba40fb4c1594bbe1
Jose-Sumoza <josesumoza440@gmail.com>
2024-10-29 00:34:05 -0400

    update post bg blurred opacity from 8% to 20%, change banner image

--- client/src/consts/banner.js
+++ client/src/consts/banner.js
@@ -1,5 +1,5 @@
 const BANNER = {
-	imgUrl: 'https://res.cloudinary.com/dqfomuqk6/image/upload/v1729458330/412953891_2197947113743985_2911372827280598069_n_qfnvoe.webp',
+	imgUrl: 'https://res.cloudinary.com/dqfomuqk6/image/upload/v1730176373/1_lcxqd6.webp',
 	title: 'Arte Original para cada espacio',
 	subtitle: 'Vea nuestras obras seleccionadas o enumere su propio trabajo para llegar a una audiencia global.'
 };
