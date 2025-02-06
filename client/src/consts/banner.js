2b829f04188b446fa2a704c809db1f0ead25d047
Jose-Sumoza <josesumoza440@gmail.com>
2025-02-06 13:43:55 -0400

    test banner title case

--- client/src/consts/banner.js
+++ client/src/consts/banner.js
@@ -1,6 +1,6 @@
 const BANNER = {
 	imgUrl: 'https://res.cloudinary.com/dqfomuqk6/image/upload/v1730176373/1_lcxqd6.webp',
-	title: 'Arte Original Para Cada Espacio',
+	title: 'ARTE ORIGINAL PARA CADA ESPACIO',
 	subtitle: 'Vea nuestras obras seleccionadas o enumere su propio trabajo para llegar a una audiencia global.'
 };
 
